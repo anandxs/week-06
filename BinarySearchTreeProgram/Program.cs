f5f1d22cde9c525cf582c07beb6426e7738c8388
Anand S <anandsatheeshk@gmail.com>
2023-06-25 18:37:50 +0530

    created node class

--- BinarySearchTreeProgram/Program.cs
+++ BinarySearchTreeProgram/Program.cs
@@ -1,6 +1,20 @@
 ﻿namespace BinarySearchTreeProgram
 {
+	public class BST
+	{
+		class Node
+		{
+			public int val { get; set; }
+			public Node? left { get; set; }
+			public Node? right { get; set; }
 
+            public Node(int val)
+            {
+                this.val = val;
+				left = right = null;
+            }
+        }
+	}
 	internal class Program
 	{
 		static void Main(string[] args)
